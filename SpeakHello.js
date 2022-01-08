e86e955b0c351193761ebebc0f86eb9b9209a1ea
Khuong <maikhuong2003@gmail.com>
2022-01-08 11:21:44 +0700

    initial commit

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+
+(function (window) {
+  /* body... */
+  var helloSpeaker = {};
+   var speakWord = "Hello";
+   helloSpeaker.speak =  function(name) {
+  console.log(speakWord + " " + name);
+}
+  window.helloSpeaker = helloSpeaker ;
+})(window);
+
